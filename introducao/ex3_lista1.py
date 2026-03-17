aa9e0e5870e41cc1a545139cf0fb47928d3d8db6
Eduardo Gondo <egondo@gmail.com>
2026-03-16 22:12:37 -0300

    Correcao dos exercicios 3 e 4

---
+++ introducao/ex3_lista1.py
@@ -0,0 +1,4 @@
+nome = input("Nome: ")
+ano = int(input("Ano de nascimento: "))
+idade = 2026 - ano
+print(nome, "tem ou tera", idade, "anos")
---
+++ introducao/ex4_lista1.py
@@ -0,0 +1,14 @@
+num_a = int(input("Valor do 1 numero: "))
+num_b = int(input("Valor do 2 numero: "))
+
+print("A soma vale", num_a + num_b)
+
+produto = num_a * num_b
+print("O produto vale", produto)
+
+quoci = num_a // num_b
+print("O resultado da divisao vale", quoci)
+
+resto = num_a % num_b
+print("O resto da divisao vale", resto)
+
