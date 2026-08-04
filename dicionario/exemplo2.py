44b0a36931b4043437d3aefadb4986eb0e9891ca
Eduardo Gondo <egondo@gmail.com>
2026-08-03 22:23:05 -0300

    Exemplos 1 e 2 de dicionários

---
+++ dicionario/exemplo2.py
@@ -0,0 +1,17 @@
+#Dicionario de listas
+
+atores = {}
+
+atores['Leonardo DiCaprio'] = ['Titanic', 'O Regresso', 'Lobo de Wall Street']
+atores['Anne Hathaway'] = ['Diabo Veste Prada', 'Diabo Veste Prada 2']
+atores['Alice Braga'] = ['Cidade de Deus', 'Eu sou a Lenda']
+
+for chave in atores:
+    print(f"Ator {chave}: ")
+    for filme in atores[chave]:
+        print(f"\t {filme}")
+
+
+atores['Anne Hathaway'].append("Diário de uma Princesa")
+
+print(atores['Anne Hathaway'])
